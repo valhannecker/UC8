0b9bef8a9a822b978699826e01146344621ba65a
Valentina Locatelli <valenhlocatelli@gmail.com>
2026-05-26 21:00:38 -0300

    Add files via upload

---
+++ MeuTerceiroProjeto/src/meuterceiroprojeto/MeuTerceiroProjeto.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package meuterceiroprojeto;
+
+/**
+ *
+ * @author VALENTINA HANNECKER LOCATELLI
+ */
+
+    public class MeuTerceiroProjeto {
+        public static void main(String[] args) {
+            String nome = "Valentina";
+            int idade = 18;
+            double altura = 1.62;
+            String cidade = "Sao Leopoldo";
+            char sexo = 'F';
+            
+            System.out.println("Dados do cadastro:");
+            System.out.println("nome: "+ nome);
+            System.out.println("idade: " + idade);
+            System.out.println("altura: " + altura);
+            System.out.println("cidade: " + cidade);
+            System.out.println("sexo: " + sexo);
+        }
+    }
+
